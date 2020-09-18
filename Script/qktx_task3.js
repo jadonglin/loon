28ca6c59ce98988771466cfbeaf0df20e3da03b6
jiadonglin <jiadonglin@t1networks.com>
2020-09-18 21:54:14 +0800

    t

--- Script/qktx_task3.js
+++ Script/qktx_task3.js
@@ -290,7 +290,10 @@ function qktx_readme()
 {
 
   newnum=qktx_rid.substring(7,15);
+  console.log("debugunewnum11:"+newnum)
+  console.log("debugurl11111:"+qktx_urlck)
 qktx_urlck=isdefined(qktx_urlck,newnum);
+console.log("debugurl11111:"+qktx_urlck)
 qktx_urlrck=isdefined(qktx_urlrck,newnum);
 }
  
