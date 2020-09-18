f8e51def907f774d4cbe14e4db5b864b4a3f3cb2
jiadonglin <jiadonglin@t1networks.com>
2020-09-18 18:39:44 +0800

    debug7

--- Script/qktx_task3.js
+++ Script/qktx_task3.js
@@ -166,7 +166,7 @@ const llUrl1 = {url:"https://appv8.qukantianxia.com/qktx-activity/activity/getEv
      console.log("debug111111111111:"+obj.result)
 
 
-     if(obj.result==1011)
+     if(1)
    {
     console.log("debug222222222:"+obj.data.firstBoxOpenStatus)
      if(obj.data.firstBoxOpenStatus==0)
